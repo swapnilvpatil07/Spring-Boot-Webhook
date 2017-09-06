dde11410de710b205f8e071eb99feb83ef75d635
swapnilvpatil07 <swapnilvpatil07@gmail.com>
2017-09-06 09:46:47 +0530

    updated

---
+++ src/main/java/com/service/Initializer.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Initializer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Initializer.class, args);
+		System.out.println("Service Started..");
+	}
+
+}
